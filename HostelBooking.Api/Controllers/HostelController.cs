8167a6ac0bd250cc96d24e440a5c6607149b8b07
Abhijit Behera <abhijitbehera207@gmail.com>
2025-12-05 13:02:48 +0530

    Add error handling to database migration to prevent app crash on startup

--- HostelBooking.Api/Controllers/HostelController.cs
+++ HostelBooking.Api/Controllers/HostelController.cs
@@ -2,9 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HostelBooking.Api.Controllers
 {
-    
+
     [Route("api/[controller]")]
     public class HostelController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "running" });
+        }
     }
 }
